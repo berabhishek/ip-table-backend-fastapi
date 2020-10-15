e598466da093cd5a7446e80576d59fd95c461548
Padma Swain <sweta.padma.swain@accenture.com>
2020-10-15 22:39:19 +0530

    addedcrud

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -2,6 +2,16 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+def create_region(db: Session, region: schemas.RegionCreate):
+    db_item = models.Region(**region.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_region(db: Session):
+    return db.query(models.Region).all()
+
 def create_country(db: Session, country: schemas.CountryCreate):
     db_item = models.Country(**country.dict())
     db.add(db_item)
@@ -10,7 +20,7 @@ def create_country(db: Session, country: schemas.CountryCreate):
     return db_item
 
 def get_country(db: Session):
-    return db.query(models.Country).all()
+    return db.query(models.Country).filters(models.Country.region == region)all()
 
 def create_city(db:Session, city: schemas.CityCreate):
     db_item = models.City(**city.dict())
@@ -21,3 +31,13 @@ def create_city(db:Session, city: schemas.CityCreate):
 
 def get_city(country: str, db: Session):
     return db.query(models.City).filter(models.City.country == country).all()
+
+def create_facility(db: Session, facility: schemas.FacilityCreate):
+    db_item = models.Facility(**facility.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_facility(db: Session):
+    return db.query(models.Facility).filters(models.Facility.city == city).all()
--- sql_app/models.py
+++ sql_app/models.py
@@ -3,10 +3,17 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+class Region(Base):
+    __tablename__ = "region"
+
+    name =  Column(String, primary_key=True)
+
 class Country(Base):
     __tablename__ = "country"
 
     name=Column(String, primary_key=True)
+
+    region = Column(String, ForeignKey("region.name") )
 
 class City(Base):
     __tablename__ = "city"
@@ -14,3 +21,15 @@ class City(Base):
     name = Column(String, primary_key=True)
 
     country = Column(String, ForeignKey("country.name"))
+
+class Facility(Base):
+    __tablename__ = "facility"
+
+    name = Column(String, primary_key=True)
+
+    city = Column(String, ForeignKey("city.name"))
+
+
+
+   
+
